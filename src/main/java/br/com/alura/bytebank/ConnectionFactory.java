893bd274ad2c868b7d54b0196cf7a3c00f7ed2d7
Jean Carlo <jeansanto2002@gmail.com>
2024-02-23 14:51:24 -0300

    Salvando contas no banco

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -4,30 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexaoDb {
+public class ConnectionFactory {
 
-    public static Connection conectar() {
+    public Connection conectar() {
         try {
             String driver = "org.postgresql.Driver";
             String url = "jdbc:postgresql://localhost:5432/bytebank";
             String user = System.getenv("DB_USER");
             String password = System.getenv("DB_PASSWORD");
             Class.forName(driver);
-            Connection conn = DriverManager.getConnection(url, user, password);
-
-            System.out.println("Conexão estabelecida");
-            return conn;
+            return DriverManager.getConnection(url, user, password);
         } catch (ClassNotFoundException | SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void main(String... x) {
-        Connection conn = conectar();
-
-        try {
-            conn.close();
-        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
